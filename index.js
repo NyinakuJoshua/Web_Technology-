54aa0d6a234b82b81cbbba683c7980b0bb0815f7
NyinakuJoshua <126934288+NyinakuJoshua@users.noreply.github.com>
2024-06-07 23:24:09 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+//CALCULATOR PROGRAM
+
+/*
+const display = document.getElementById("display");
+
+function appendToDiaplay(input){
+    display.value += input;
+}
+
+function calculate(){
+   try{
+    display.value =eval(display.value)
+   }
+   catch(error){
+    display.value = "Error"
+   }
+}
+*/
